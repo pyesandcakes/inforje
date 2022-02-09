44598d3feaa4f8d663cf3f9e73f734f77aaf6847
pyesandcakes <95701949+pyesandcakes@users.noreply.github.com>
2022-02-09 09:24:07 +0100

    Update 1.zadatak.py

--- DRUGI TEST/1.zadatak.py
+++ DRUGI TEST/1.zadatak.py
@@ -1,3 +1,10 @@
+#Upiši prirodan broj n, te n prirodnih brojeva koje spremamo u listu l. Napravi sljedeće radnje u ovom poretku (nakon svake radnje isprintajte listu:
+#1.Izbaci najveći element
+#2.Sortiraj listu
+#3.Dodaj najmani element liste na prvo mjesto
+#4.Dodaj listi[1,7,8] elemente liste l na kraj
+#5.Printaj na kojem mjestu se nalazi najveći broj
+#6.Printaj koliko ima sedmica u listi te koliko lista ima elemenata.
 n=int(input('unesi broj brojeva'))
 l=[]
 b=[1,7,8]
