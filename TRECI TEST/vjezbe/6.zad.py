37be90e9c8eb941ae34180f21a0e669c8c93c75d
pyesandcakes <95701949+pyesandcakes@users.noreply.github.com>
2022-03-28 20:00:10 +0200

    bolje

--- TRECI TEST/vjezbe/6.zad.py
+++ TRECI TEST/vjezbe/6.zad.py
@@ -1,4 +1,5 @@
+#Upišite string S koji je gramatička rečenica bez gramatičkih znakova. Preslozite rijeci po njihovim duljinama.
 s=input('unesi recenicu bez gramatickih znakova')
-ind=s.split()
-ind2=sorted(ind, key=len)
-print(' '.join(ind2))           
+l=s.split()
+z=sorted(l, key=len)
+print(' '.join(z))           
