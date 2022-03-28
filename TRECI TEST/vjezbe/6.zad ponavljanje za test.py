e8531cb1670e7231fdb750ed37a7008869bd5260
pyesandcakes <95701949+pyesandcakes@users.noreply.github.com>
2022-03-28 19:26:29 +0200

    Update 6.zad ponavljanje za test.py

--- TRECI TEST/vjezbe/6.zad ponavljanje za test.py
+++ TRECI TEST/vjezbe/6.zad ponavljanje za test.py
@@ -1,3 +1,4 @@
+#Upišite string S koji je gramatička rečenica bez gramatičkih znakova. Izbacite najdulju riječ iz stringa S.
 s=input('unesi recenicu bez gramatickih znakova ')
 longest = max(s.split(), key=len)
 s=s.replace(longest, '')
