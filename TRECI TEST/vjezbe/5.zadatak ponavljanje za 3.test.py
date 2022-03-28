549f731751641d022e28096092b6d868fe63c4c1
pyesandcakes <95701949+pyesandcakes@users.noreply.github.com>
2022-03-28 19:12:35 +0200

    Update 5.zadatak ponavljanje za 3.test.py

--- TRECI TEST/vjezbe/5.zadatak ponavljanje za 3.test.py
+++ TRECI TEST/vjezbe/5.zadatak ponavljanje za 3.test.py
@@ -1,9 +1,5 @@
 #Upišite string S koji je gramatička rečenica bez gramatičkih znakova. Obrnite svaku riječ.
 s=input('unesi recenicu bez recenicnih znakova')
-s=s.split(' ')
-p=''
-for i in s:
-    rev=''.join(reversed(i))
-    p+=rev
-    p+=' '
-print(p)
+stringlength=len(s) # calculate length of the list
+slicedString=s[stringlength::-1] # slicing 
+print (slicedString) # print the reversed string
